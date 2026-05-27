9c6429bcb26a5a778196e23e5805c43ca5d5e000
FabianPonczko <fabianponczko@live.com.ar>
2026-05-27 14:16:54 -0300

    cambio en main

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,9 +1,11 @@
 const admin = require('firebase-admin');
+require('dotenv').config();
 
-const serviceAccount = require(
-  '../serviceAccountKey.json'
-);
 
+// const serviceAccount = require(
+//   '../serviceAccountKey.json'
+// );
+const serviceAccount =process.env.serviceAccount
 admin.initializeApp({
   credential:
     admin.credential.cert(
