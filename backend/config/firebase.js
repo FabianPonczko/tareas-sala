8f212e3415334d5d18021dbc07833f425eb74cd8
FabianPonczko <fabianponczko@live.com.ar>
2026-05-29 10:24:32 -0300

    json.parce google key 3

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,28 +1,16 @@
-
 const admin = require('firebase-admin');
 require('dotenv').config();
 
-let serviceAccount;
 
-try {
-  if (!process.env.SERVICE_ACOOUNT) {
-    // Esto evitará que JSON.parse rompa el servidor y te dará un log claro
-    console.error("❌ ERROR CRÍTICO: La variable 'SERVICE_ACOOUNT' no está llegando al servidor.");
-    process.exit(1); 
-  }
-
-  serviceAccount = JSON.parse(process.env.SERVICE_ACOOUNT);
-  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
-
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-  
-  console.log("✅ Firebase Admin inicializado correctamente.");
-
-} catch (error) {
-  console.error("❌ Error al parsear o inicializar Firebase:", error.message);
-  process.exit(1);
-}
+// const serviceAccount = require(
+//   '../serviceAccountKey.json'
+// );
+const serviceAccount = process.env.SERVICE_ACOOUNT
+admin.initializeApp({
+  credential:
+    admin.credential.cert(
+      serviceAccount
+    ),
+});
 
 module.exports = admin;
