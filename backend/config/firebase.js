9975da162d233db61f170ad6d6e351c9566546bf
FabianPonczko <fabianponczko@live.com.ar>
2026-05-29 11:58:54 -0300

    vuelvo el server.js

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // const serviceAccount = require(
 //   '../serviceAccountKey.json'
 // );
-const serviceAccount = process.env.SERVICE_ACOOUNT
+const serviceAccount = JSON.parse(process.env.SERVICE_ACOOUNT)
 admin.initializeApp({
   credential:
     admin.credential.cert(
