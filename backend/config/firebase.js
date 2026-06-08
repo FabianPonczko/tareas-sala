fb290a7c2c2aac9f879d79aa4c891cf75b319ace
FabianPonczko <fabianponczko@live.com.ar>
2026-06-07 21:40:10 -0300

    test backend

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -6,6 +6,15 @@ require('dotenv').config();
 //   '../serviceAccountKey.json'
 // );
 const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT)
+console.log(
+  'Firebase Project:',
+  serviceAccount.project_id
+);
+
+console.log(
+  'Firebase Email:',
+  serviceAccount.client_email
+);
 admin.initializeApp({
   credential:
     admin.credential.cert(
